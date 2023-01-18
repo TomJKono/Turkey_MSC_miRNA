03ef4b059eca7a7a02fe0102d8a137f728ba7fca
Tom Kono <konox006@umn.edu>
2023-01-18 08:58:33 -0600

    Update BAM linker

--- Scripts/Link_BAM.sh
+++ Scripts/Link_BAM.sh
@@ -4,9 +4,15 @@
 # long ugly file paths.
 
 # Directory to where the BAM files are
-SRC_DIR="/home/reedkm/shared/RIS_Projects/Reed_Project_016/miRNA_Expression/BAM"
+#   These are for STAR
+# SRC_DIR="/home/reedkm/shared/RIS_Projects/Reed_Project_016/miRNA_Expression/BAM"
+#   These are for Bowtie
+SRC_DIR="/home/reedkm/shared/RIS_Projects/Reed_Project_016/miRNA_Expression/BAM_BT"
 # Directory to where the links will be made
-DEST_DIR="/home/reedkm/shared/RIS_Projects/Reed_Project_016/miRNA_Expression/BAM_NiceNames"
+#   For STAR
+# DEST_DIR="/home/reedkm/shared/RIS_Projects/Reed_Project_016/miRNA_Expression/BAM_NiceNames"
+#   For Bowtie
+DEST_DIR="/home/reedkm/shared/RIS_Projects/Reed_Project_016/miRNA_Expression/BAM_BT_NiceNames"
 
 mkdir -p "${DEST_DIR}"
 cd "${DEST_DIR}"
@@ -14,6 +20,9 @@ cd "${DEST_DIR}"
 # Make the links!
 for bfile in $(find "${SRC_DIR}" -mindepth 1 -maxdepth 1 -type f -name '*.bam')
 do
-    sname=$(basename "${bfile}" | sed -e 's/_Aligned.sortedByCoord.out.bam//g')
+    # for STAR filenames
+    # sname=$(basename "${bfile}" | sed -e 's/_Aligned.sortedByCoord.out.bam//g')
+    # for Bowtie filenames
+    sname=$(basename "${bfile}" | sed -e 's/.bam//g')
     ln -s "${bfile}" "./${sname}"
 done
